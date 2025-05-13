e221788bbf6e296ecdeb5f07b7e4cb8a9d463e2d
Mohamed Afifi <afifi@layout.dev>
2025-05-12 21:59:50 -0400

    Use PascalCase for page names

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Index from "@/pages/index";
-import NotFound from "@/pages/not-found";
+import Index from "@/pages/Index";
+import NotFound from "@/pages/NotFound";
 
 const queryClient = new QueryClient();
 

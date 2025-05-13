922c263685496fc9eca1a927a34cb15ed6f9e07a
Mohamed Afifi <afifi@layout.dev>
2025-05-12 21:51:57 -0400

    Add a fallback not found page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Index from "@/pages/index";
+import NotFound from "@/pages/not-found";
 
 const queryClient = new QueryClient();
 
@@ -16,6 +17,7 @@ function App() {
           <Sonner />
           <Routes>
             <Route path="/" element={<Index />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
